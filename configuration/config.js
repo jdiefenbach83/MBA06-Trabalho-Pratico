f98728fb25068113458fc3d4b5a37b3508bf3fa2
Jefferson Diefenbach <jefferson.diefenbach@gmail.com>
2021-02-24 21:18:57 -0300

    Trabalho pratico

---
+++ configuration/config.js
@@ -0,0 +1,8 @@
+module.exports={
+    "google_api_key": "yourKey.apps.googleusercontent.com",
+    "google_api_secret": "secretKey",
+    "google_callback_url": "http://localhost:3000/auth/google/callback",
+    "facebook_api_key": "",
+    "facebook_api_secret": "",
+    "facebook_callback_url": "http://localhost:3000/auth/facebook/callback",
+}
